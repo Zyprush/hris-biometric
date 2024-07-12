1f3305aea68abd71a4ec8258a2fa301c18006d35
zanzen <olsencortuna@gmail.com>
2024-07-12 21:47:23 +0800

    to fix: navbar user

--- src/components/UserLayout.tsx
+++ src/components/UserLayout.tsx
@@ -31,37 +31,84 @@ const UserLayout: React.FC<NavbarProps> = ({ children }) => {
   };
 
   return (
-    <div className="h-screen w-screen flex flex-col md:flex-row bg-slate-100 overflow-hidden">
+    <div>
+    <div className="h-screen w-screen flex flex-col bg-slate-100 overflow-hidden">
+      <span className="w-full h-14 bg-zinc-200 justify-between px-5 items-center border-b-2 border-zinc-300 hidden md:flex">
+        <button
+          onClick={toggleNavbar}
+          data-tip="toggle width"
+          className={`text-2xl text-zinc-700 flex tooltip tooltip-right p-2 font-bold rounded-md gap-2`}
+        >
+          <LuFingerprint className="text-3xl" />
+          {!isMinimized && 
+          <p className="bg-neutral px-2 rounded-md text-white">HRIS</p>}
+        </button>
+        <span className="h-10 w-10 flex bg-zinc-500 rounded-full "></span>
+      </span>
       <div
         className={`flex ${
           isMinimized ? "w-20" : "w-56"
         } h-screen bg-zinc-200 transition-width duration-300 justify-start items-start hidden md:flex`}
       >
-        <nav className="flex flex-col items-start justify-start p-5 gap-2 mx-auto">
-          <button
-            onClick={toggleNavbar}
-            data-tip="toggle width"
-            className={`text-2xl text-zinc-700 flex mb-5 tooltip tooltip-right m-auto p-2 font-bold rounded-md gap-2`}
+        <nav className="flex flex-col items-start justify-start p-5 gap-2">
+          <Link
+            href={"/user/dashboard"}
+            className={`navlink ${
+              pathname === "/user/dashboard"
+                ? "bg-neutral text-white"
+                : "text-zinc-700"
+            }`}
           >
-            <LuFingerprint className="text-3xl" /> {!isMinimized && <p className="bg-neutral px-2 rounded-md text-white">HRIS</p>}
-          </button>
-          <Link href={"/user/dashboard"} className={`navlink ${pathname === "/user/dashboard" ? "bg-neutral text-white" : "text-zinc-700"}`}>
             <BsBarChartFill className="text-xl" /> {!isMinimized && "Dashboard"}
           </Link>
-          <Link href={"/user/attendance"} className={`navlink ${pathname === "/user/attendance" ? "bg-neutral text-white" : "text-zinc-700"}`}>
+          <Link
+            href={"/user/attendance"}
+            className={`navlink ${
+              pathname === "/user/attendance"
+                ? "bg-neutral text-white"
+                : "text-zinc-700"
+            }`}
+          >
             <MdTry className="text-xl" /> {!isMinimized && "Attendance"}
           </Link>
-          <Link href={"/user/payslip"} className={`navlink ${pathname === "/user/payroll" ? "bg-neutral text-white" : "text-zinc-700"}`}>
+          <Link
+            href={"/user/payslip"}
+            className={`navlink ${
+              pathname === "/user/payroll"
+                ? "bg-neutral text-white"
+                : "text-zinc-700"
+            }`}
+          >
             <MdPayments className="text-xl" /> {!isMinimized && "Payroll"}
           </Link>
-          <Link href={"/user/history"} className={`navlink ${pathname === "/user/history" ? "bg-neutral text-white" : "text-zinc-700"}`}>
+          <Link
+            href={"/user/history"}
+            className={`navlink ${
+              pathname === "/user/history"
+                ? "bg-neutral text-white"
+                : "text-zinc-700"
+            }`}
+          >
             <RiFolderHistoryFill className="text-xl" />{" "}
             {!isMinimized && "History"}
           </Link>
-          <Link href={"/user/account"} className={`navlink ${pathname === "/user/account" ? "bg-neutral text-white" : "text-zinc-700"}`}>
+          <Link
+            href={"/user/account"}
+            className={`navlink ${
+              pathname === "/user/account"
+                ? "bg-neutral text-white"
+                : "text-zinc-700"
+            }`}
+          >
             <FaUserCircle className="text-xl" /> {!isMinimized && "Account"}
           </Link>
-          <button className="navlink text-zinc-700" onClick={() => { auth.signOut(); router.push("/sign-in"); }}>
+          <button
+            className="navlink text-zinc-700"
+            onClick={() => {
+              auth.signOut();
+              router.push("/sign-in");
+            }}
+          >
             <FaSignOutAlt className="text-xl" /> {!isMinimized && "Logout"}
           </button>
         </nav>
@@ -72,31 +119,73 @@ const UserLayout: React.FC<NavbarProps> = ({ children }) => {
             <LuFingerprint className="text-3xl" />
             <p className="bg-neutral px-2 rounded-md text-white">HRIS</p>
           </div>
-          <button
-            onClick={toggleMobileMenu}
-            className="text-2xl text-zinc-700"
-          >
-            {isMobileMenuOpen ? <FaTimes className="text-xl" /> : <FaBars className="text-xl" />}
+          <button onClick={toggleMobileMenu} className="text-2xl text-zinc-700">
+            {isMobileMenuOpen ? (
+              <FaTimes className="text-xl" />
+            ) : (
+              <FaBars className="text-xl" />
+            )}
           </button>
         </div>
         {isMobileMenuOpen && (
-          <nav className="flex flex-col items-start justify-start p-5 gap-2 bg-zinc-200">
-            <Link href={"/user/dashboard"} className={`navlink ${pathname === "/user/dashboard" ? "bg-neutral text-white" : "text-zinc-700"}`}>
+          <nav className={`flex flex-col items-center justify-start p-5 gap-2 transition-all duration-500 ${isMobileMenuOpen ? 'h-screen' : 'h-0'}`}>
+            <Link
+              href={"/user/dashboard"}
+              className={`navlink ${
+                pathname === "/user/dashboard"
+                  ? "bg-neutral text-white"
+                  : "text-zinc-700"
+              }`}
+            >
               <BsBarChartFill className="text-xl" /> Dashboard
             </Link>
-            <Link href={"/user/attendance"} className={`navlink ${pathname === "/user/attendance" ? "bg-neutral text-white" : "text-zinc-700"}`}>
+            <Link
+              href={"/user/attendance"}
+              className={`navlink ${
+                pathname === "/user/attendance"
+                  ? "bg-neutral text-white"
+                  : "text-zinc-700"
+              }`}
+            >
               <MdTry className="text-xl" /> Attendance
             </Link>
-            <Link href={"/user/payslip"} className={`navlink ${pathname === "/user/payroll" ? "bg-neutral text-white" : "text-zinc-700"}`}>
+            <Link
+              href={"/user/payslip"}
+              className={`navlink ${
+                pathname === "/user/payroll"
+                  ? "bg-neutral text-white"
+                  : "text-zinc-700"
+              }`}
+            >
               <MdPayments className="text-xl" /> Payroll
             </Link>
-            <Link href={"/user/history"} className={`navlink ${pathname === "/user/history" ? "bg-neutral text-white" : "text-zinc-700"}`}>
+            <Link
+              href={"/user/history"}
+              className={`navlink ${
+                pathname === "/user/history"
+                  ? "bg-neutral text-white"
+                  : "text-zinc-700"
+              }`}
+            >
               <RiFolderHistoryFill className="text-xl" /> History
             </Link>
-            <Link href={"/user/account"} className={`navlink ${pathname === "/user/account" ? "bg-neutral text-white" : "text-zinc-700"}`}>
+            <Link
+              href={"/user/account"}
+              className={`navlink ${
+                pathname === "/user/account"
+                  ? "bg-neutral text-white"
+                  : "text-zinc-700"
+              }`}
+            >
               <FaUserCircle className="text-xl" /> Account
             </Link>
-            <button className="navlink text-zinc-700" onClick={() => { auth.signOut(); router.push("/sign-in"); }}>
+            <button
+              className="navlink text-zinc-700"
+              onClick={() => {
+                auth.signOut();
+                router.push("/sign-in");
+              }}
+            >
               <FaSignOutAlt className="text-xl" /> Logout
             </button>
           </nav>
@@ -104,6 +193,8 @@ const UserLayout: React.FC<NavbarProps> = ({ children }) => {
       </div>
       <div className="flex-1 overflow-y-auto">{children}</div>
     </div>
+    </div>
+
   );
 };
 
