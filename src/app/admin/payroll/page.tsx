2ae83e9b296bede38dcea1778f501e9bfe15656a
Zyprush <alberiojake27@gmail.com>
2024-07-16 15:59:34 +0800

    added placeholder for payroll

--- src/app/admin/payroll/page.tsx
+++ src/app/admin/payroll/page.tsx
@@ -1,40 +1,54 @@
 "use client";
+
+import AdminRouteGuard from "@/app/AdminRouteGuard/page";
 import AdminLayout from "@/components/AdminLayout";
-import Loading from "@/components/Loading";
 import { SignedIn } from "@/components/signed-in";
-import { auth } from "@/firebase";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
+import { useState } from "react";
+import { BsPersonBoundingBox, BsPersonCircle } from "react-icons/bs";
+import Payroll from "./Payroll";
+import Deduction from "./Deduction";
 
 const AdminPayroll = () => {
-  const [user, loading] = useAuthState(auth);
-  const router = useRouter();
+  const [currentTab, setCurrentTab] = useState("Payroll");
 
-  const setAuthChecked = (isChecked: boolean) => {
-    console.log("Auth checked:", isChecked);
+  const renderContent = () => {
+    switch (currentTab) {
+      case "Payroll":
+        return <Payroll />;
+      case "Deduction":
+        return <Deduction />;
+      default:
+        return <Payroll />;
+    }
   };
 
-  useEffect(() => {
-    if (!loading) {
-      if (!user) {
-        console.log("User not authenticated. Redirecting to sign-in page...");
-        router.push("../sign-in");
-      }
-      setAuthChecked(true); 
-    }
-  }, [user, loading, router]);
-
-  if (loading) {
-    return <Loading />;
-  }
-
   return (
-    <SignedIn>
-      <AdminLayout>
-        <div>Payroll</div>
-      </AdminLayout>
-    </SignedIn>
+    <AdminRouteGuard>
+      <SignedIn>
+        <AdminLayout>
+          <div className="container h-full mx-auto p-4">
+            <div className="grid grid-col-1 py-4">
+              <p className="text-lg font-bold">{currentTab}</p>
+              <div className="join rounded-md my-5">
+                <button
+                  className={`btn join-item border-2 border-zinc-400 ${currentTab === "Payroll" ? "bg-primary text-white border-primary" : ""}`}
+                  onClick={() => setCurrentTab("Payroll")}
+                >
+                  <BsPersonBoundingBox className="text-base" /> Payroll
+                </button>
+                <button
+                  className={`btn join-item border-2 border-zinc-400 ${currentTab === "Deduction" ? "bg-primary text-white border-primary" : ""}`}
+                  onClick={() => setCurrentTab("Deduction")}
+                >
+                  <BsPersonCircle className="text-base" /> Deduction
+                </button>
+              </div>
+              {renderContent()}
+            </div>
+          </div>
+        </AdminLayout>
+      </SignedIn>
+    </AdminRouteGuard>
   );
 };
 
