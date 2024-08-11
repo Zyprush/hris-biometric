f0c0debd26bf614b619003c6379716c92a9cf0f3
zanzen <olsencortuna@gmail.com>
2024-08-11 14:02:22 +0800

    fix notifications

--- src/components/AdminNotifications.tsx
+++ src/components/AdminNotifications.tsx
@@ -1,8 +1,5 @@
 "use client";
 import React, { Dispatch, SetStateAction } from "react";
-import { FaMessage } from "react-icons/fa6";
-import animationData from "../../public/img/read-animation.json";
-import Lottie from "react-lottie";
 import Link from "next/link";
 import { IoIosCloseCircle } from "react-icons/io";
 
@@ -16,20 +13,12 @@ const AdminNotifications: React.FC<NotificationsProps> = ({
   setShowNotif,
   text,
 }) => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
+
   return (
     <div className="fixed max-w-[22rem] min-h-10 right-4 bottom-10 top-auto left-auto inset-0 flex gap-4">
       <span className="bg-white border relative flex flex-row gap-10 rounded-lg p-4 h-auto text-zinc-600 mb-10 mr-4 m-auto shadow-xl  max-w-[22rem]  min-w-[22rem] custom-shadow2 z-50">
         <Link className="flex gap-2" href={"/admin/attendance"}>
           <span className="flex">
-            <Lottie options={defaultOptions} height={60} width={70} />
           </span>
           <p className="text-sm my-auto">
             {text}
